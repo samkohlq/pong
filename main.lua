7d0ae9cb25085728ad119e88147e27e068afb441
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-04-15 23:41:31 +0800

    add push resolution-handling library, load game with 'Point' filtering

--- main.lua
+++ main.lua
@@ -1,18 +1,37 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
+push = require 'push' -- import functions from push.lua
+
+-- initialise the game when it first starts
 function love.load()
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+  love.graphics.setDefaultFilter('nearest', 'nearest') -- set 'Point' filtering for retro aesthetic
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
-    -- sync to monitor's refresh rate
     vsync = true,
-    -- prevent user from scaling window after programme has started
     resizable = false
   })
 end
 
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit() -- allow user to quit game by pressing 'escape' key
+  end
+end
+
+-- update the screen
 function love.draw()
-  -- centre vertically by dividing height by two and moving up 6 pixels, the height of the text
-  love.graphics.printf("hello world", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+  push:apply('start') -- draw things the 'push' way
+
+  love.graphics.printf(
+    "hello world", -- text to render
+    0, -- starting X position; 0 since we centre the text based on width
+    VIRTUAL_HEIGHT / 2 - 6, -- starting Y position; height divided by two and moved up 6 pixels, the height of the text
+    VIRTUAL_WIDTH, -- number of pixels to centre within entire screen
+    'center') -- alignment mode
+  
+  push:apply('end')
 end
