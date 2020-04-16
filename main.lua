f7f5b9febbe9a9cf2cf5df5985b0a21498ad792f
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-04-16 12:58:43 +0800

    add bitmap font, draw player paddles and ball

--- main.lua
+++ main.lua
@@ -9,6 +9,11 @@ push = require 'push' -- import functions from push.lua
 -- initialise the game when it first starts
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest') -- set 'Point' filtering for retro aesthetic
+  
+  -- set font
+  smallFont = love.graphics.newFont('04B_03__.ttf', 8)
+  love.graphics.setFont(smallFont)
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     vsync = true,
@@ -26,10 +31,18 @@ end
 function love.draw()
   push:apply('start') -- draw things the 'push' way
 
+  love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255) -- divide by 255 because range of each is from 0 - 1
+
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 3, VIRTUAL_HEIGHT / 2 - 3, 6, 6) -- draw the ball and centre it
+
+  love.graphics.rectangle('line', 5, 20, 5, 20) -- draw player 1's paddle
+
+  love.graphics.rectangle('line', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20) -- draw player 2's paddle
+
   love.graphics.printf(
     "hello world", -- text to render
     0, -- starting X position; 0 since we centre the text based on width
-    VIRTUAL_HEIGHT / 2 - 6, -- starting Y position; height divided by two and moved up 6 pixels, the height of the text
+    20, -- starting Y position;
     VIRTUAL_WIDTH, -- number of pixels to centre within entire screen
     'center') -- alignment mode
   
